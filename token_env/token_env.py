f4e189defafa346371568808ef1ec99934f45e3b
beyazit-y <beyazit.yalcinkaya@gmail.com>
2025-07-11 14:13:53 -0700

    fix: fix slip_prob typo/bug

--- token_env/token_env.py
+++ token_env/token_env.py
@@ -23,17 +23,15 @@ class TokenEnv(gym.Env):
         self.action_space = spaces.Discrete(4)
         self.observation_space = spaces.Box(low=0, high=1, shape=(self.n_tokens, *self.size), dtype=np.uint8)
         self.t = 0
-        self.agent = None
         self.action_map = {0: (1, 0), 1: (0, 1), 2: (-1, 0), 3: (0, -1)}
+        self.agent = None
         if self.use_fixed_map:
-            # _, self.tokens = self._sample_map()
+            self.agent = (0, 0)
             self.tokens = [(0, np.array([5, 5])), (1, np.array([2, 4])), (2, np.array([3, 5])), (3, np.array([2, 2])), (4, np.array([0, 2])), (5, np.array([6, 4])), (6, np.array([0, 5])), (7, np.array([5, 1])), (8, np.array([6, 2])), (9, np.array([2, 3])), (0, np.array([0, 1])), (1, np.array([3, 0])), (2, np.array([5, 0])), (3, np.array([0, 0])), (4, np.array([1, 4])), (5, np.array([2, 0])), (6, np.array([4, 2])), (7, np.array([5, 4])), (8, np.array([3, 1])), (9, np.array([1, 1]))]
 
     def reset(self, seed: int | None = None, options: dict[str, Any] | None = None) -> tuple[np.ndarray, dict[str, Any]]:
         np.random.seed(seed)
-        if self.use_fixed_map:
-            self.agent = (0, 0)
-        else:
+        if not self.use_fixed_map:
             self.agent, self.tokens = self._sample_map()
         self.t = 0
         obs = self._get_obs()
@@ -42,12 +40,12 @@ class TokenEnv(gym.Env):
     def step(self, action: int) -> tuple[np.ndarray, int, bool, bool, dict[str, Any]]:
         dx, dy = self.action_map[action]
         dx_slip = int(np.sign(self.slip_prob[0])) if np.random.random() < abs(self.slip_prob[0]) else 0
-        dy_slip = int(np.sign(self.slip_prob[1])) if np.random.random() < abs(self.slip_prob[0]) else 0
+        dy_slip = int(np.sign(self.slip_prob[1])) if np.random.random() < abs(self.slip_prob[1]) else 0
         agent_x = (self.agent[0] + dx + dx_slip + self.size[0]) % self.size[0]
         agent_y = (self.agent[1] + dy + dy_slip + self.size[1]) % self.size[1]
         self.agent = (agent_x, agent_y)
         obs = self._get_obs()
-        reward = 0
+        reward = None
         self.t += 1
         done = self.t >= self.timeout
         return obs, reward, done, False, {}
